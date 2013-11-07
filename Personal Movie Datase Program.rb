853a67fc53be282fb693026be204a20207fffe5f
cdrewsgo <chuckdrews@outlook.com>
2013-11-06 19:42:24 -0600

    Delete Personal Movie Datase Program.rb

--- Personal Movie Datase Program.rb
+++
@@ -1,8 +0,0 @@
-movies = {Tangled: 1,
-The_Godfather: 4,
-This_is_The_End: 2,
-Carlitos_way: 3}
-
-puts "What would you like to do?"
-puts "To Add a movie, Update your movies, Display your movies, or Delete your movies" 
-choice = gets.chomp
